e8a871d489e535a6aebbaf22a155c5f6faae708d
jazzdragon <a.dahari@contactelevation.com>
2019-10-11 16:39:30 -0700

    Refactor component to be functional

--- src/views/Launches.jsx
+++ src/views/Launches.jsx
@@ -1,40 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ConnectedView from './ConnectedView';
 import {fetchLaunchesIfNeeded} from "../actions/Launches";
 import Launch from '../components/Launch';
 
-class LaunchesView extends Component {
-  componentDidMount() {
-    const { dispatch, launchesCollection } = this.props;
+const LaunchesView = ({ dispatch, launchesCollection, launchCollection }) => {
+  useEffect(() => {
     fetchLaunchesIfNeeded({ dispatch, launchesCollection });
+  }, [fetchLaunchesIfNeeded])
+
+  if (!launchCollection || launchCollection.fetching) {
+    return <div> LOADING </div>;
   }
 
-  getContent() {
-    const { launchCollection } = this.props;
-
-    if (!launchCollection || launchCollection.fetching) {
-      return <div> LOADING </div>;
-    }
-
-    if (!launchCollection.launches.length) {
-      return <div> NO DATA </div>;
-    }
-
-    const launches = launchCollection.launches.map((launch) =>
-        <Launch key={ launch.flight_number } launch={launch} />
-    );
-
-    return <ul>{launches}</ul>;
+  if (!launchCollection.launches.length) {
+    return <div> NO DATA </div>;
   }
 
-  render() {
-    return (
-      <div>
-        <h2> SpaceX launches </h2>
-        {this.getContent()}
-      </div>
-    );
-  }
+  const launches = launchCollection.launches.map((launch) =>
+      <Launch key={ launch.flight_number } launch={launch} />
+  );
+
+  return (
+    <div>
+      <h2> SpaceX launches </h2>
+      <ul>
+        {launches}
+      </ul>
+    </div>
+  );
 }
 
 export default ConnectedView(LaunchesView, 'launches');
